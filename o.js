7127a6fd1d4f33a94434e6c4fb7b32a2e8672b14
Konstantin <unegade@gmail.com>
2024-01-07 10:29:50 +0500

    добавил кнопку

--- o.js
+++ o.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function render_trailers(card) {
+    function render_trailers(filmName) {
 		// Lampa.Select.show({
 		// 	title: 'Плейлист',
 		// 	items: [{title: "<div><img src='https://i.ytimg.com/vi/Hi8wbejJ1fA/mqdefault.jpg' />qew</div>", id: "Hi8wbejJ1fA", html: "<div>https://i.ytimg.com/vi/Hi8wbejJ1fA/mqdefault.jpg</div>"}],
@@ -13,11 +13,11 @@
 		// 		Lampa.Controller.toggle('content');
 		// 	}
 		// }); 
-		var Q = card;
-		var name = $('.full-start-new__title', card).text();
-		console.log(card);
-		console.log(name);
-        Lampa.Noty.show(name);
+		// var Q = card;
+		// var name = $('.full-start-new__title', card).text();
+		// console.log(card);
+		console.log(filmName);
+        Lampa.Noty.show(filmName);
     }
 
 
@@ -26,9 +26,10 @@
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
 				var render = e.object.activity.render();
+				var filmName = $('.full-start-new__title', card).text();
                 $('.full-start-new__buttons', render).append('<div class="full-start__button selector button--trailer"><svg height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="1.5" y="1.5" width="25" height="31" rx="2.5" stroke="currentColor" stroke-width="3"></rect><rect x="6" y="7" width="9" height="9" rx="1" fill="currentColor"></rect><rect x="6" y="19" width="16" height="3" rx="1.5" fill="currentColor"></rect><rect x="6" y="25" width="11" height="3" rx="1.5" fill="currentColor"></rect><rect x="17" y="7" width="5" height="3" rx="1.5" fill="currentColor"></rect> </svg><span>Трейлеры</span></div>');
                 $('.button--trailer', render).on('hover:enter', function (card) {
-                    render_trailers(card);
+                    render_trailers(filmName);
                 });
             }
 
