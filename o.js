bd70b069dceecc398d0feaa964d0580691f3a318
Konstantin <unegade@gmail.com>
2024-01-07 11:27:17 +0500

    добавил кнопку

--- o.js
+++ o.js
@@ -9,7 +9,7 @@
 		});
 	}
 
-	function render_trailers(filmName) {
+	function find_trailers(filmName) {
 		var token = 'AIzaSyDIWii53ZrXWVTEMOWf6lX2Ix347eFPTww';
 		var searchName = filmName + ' русский трейлер';
 		$.get('https://www.googleapis.com/youtube/v3/search?key=' + token + '&q=' + searchName + '&part=snippet&maxResults=10', function (data) {
@@ -43,12 +43,12 @@
 		window.trailer_plugin = true;
 		Lampa.Listener.follow('full', function (e) {
 			if (e.type == 'complite') {
-				e.object.activity.render().find('.view--trailer').remove();
 				var render = e.object.activity.render();
+				render.find('.view--trailer').remove();  // удаление дефолтных трейлеров
 				var filmName = $('.full-start-new__title', render).text();
 				$('.full-start-new__buttons', render).prepend('<div class="full-start__button selector button--trailer"><svg height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="1.5" y="1.5" width="25" height="31" rx="2.5" stroke="currentColor" stroke-width="3"></rect><rect x="6" y="7" width="9" height="9" rx="1" fill="currentColor"></rect><rect x="6" y="19" width="16" height="3" rx="1.5" fill="currentColor"></rect><rect x="6" y="25" width="11" height="3" rx="1.5" fill="currentColor"></rect><rect x="17" y="7" width="5" height="3" rx="1.5" fill="currentColor"></rect> </svg><span>Трейлеры</span></div>');
 				$('.button--trailer', render).on('hover:enter', function (card) {
-					render_trailers(filmName);
+					find_trailers(filmName);
 				});
 			}
 
