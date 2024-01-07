cf3af7791dc749160c4615724ce82cb3812259f8
Konstantin <unegade@gmail.com>
2024-01-07 17:47:25 +0500

    добавил кнопку

--- o.js
+++ o.js
@@ -22,8 +22,8 @@
 					var id = item['id']['videoId'];
 					var title = item['snippet']['title'];
 					var image = item['snippet']['thumbnails']['medium']['url'];
-					var html = '<div><img src="' + image + '"/>'+ title + '</div>'
-					return {id: id, title: html}
+					var html = '<div><img src="' + image + '"/>' + title + '</div>'
+					return { id: id, title: html }
 				}),
 				onSelect: function onSelect(a) {
 					Lampa.YouTube.play(a.id)
@@ -38,9 +38,21 @@
 		// Lampa.Noty.show(filmName);
 	}
 
+	function init_settings() {
+		var icon_trailer_settings = '<div class="settings-folder" style="padding:0!important"><div style="width:1.8em;height:1.3em;padding-right:.5em"><svg viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32"><path d="m17 14.5 4.2-4.5L4.9 1.2c-.1-.1-.3-.1-.6-.2L17 14.5zM23 21l5.9-3.2c.7-.4 1.1-1 1.1-1.8s-.4-1.5-1.1-1.8L23 11l-4.7 5 4.7 5zM2.4 1.9c-.3.3-.4.7-.4 1.1v26c0 .4.1.8.4 1.2L15.6 16 2.4 1.9zM17 17.5 4.3 31c.2 0 .4-.1.6-.2L21.2 22 17 17.5z" fill="currentColor" fill="#ffffff" class="fill-000000"></path></svg></div><div style="font-size:1.3em">Онлайн</div></div>';
+		Lampa.SettingsApi.addComponent({
+			component: 'trailer_settings',
+			name: 'Трейлеры',
+			icon: icon_trailer_settings
+		});
+	}
+
 
 	function startPlugin() {
 		window.trailer_plugin = true;
+		Lampa.Listener.follow('app', function (e) {
+			if (e.type === 'ready') init_settings()
+		});
 		Lampa.Listener.follow('full', function (e) {
 			if (e.type == 'complite') {
 				var render = e.object.activity.render();
